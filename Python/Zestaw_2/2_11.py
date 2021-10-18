d3da1ffa2299dbc71207913164c749df03e9b68f
Konrad <80765461+kon-pas@users.noreply.github.com>
2021-10-19 00:30:26 +0200

    Create 2_11.py

---
+++ Python/Zestaw_2/2_11.py
@@ -0,0 +1,8 @@
+""" 2.11 Funkcja wyswietla napis, ktory przyjmuje jako argument. Wszystkie 
+znaki napisu oddzielone sa od siebie znakiem podlogi '_' """
+def split_letters(_string):
+	return '_'.join(_string[i] for i in range(0, len(_string)))
+
+
+if __name__ == "__main__":
+	print(split_letters("abcd"))
