44e42e0f11043a30dee776a080327f3018d3e81c
Konrad <80765461+kon-pas@users.noreply.github.com>
2021-10-19 00:31:00 +0200

    Update 2_11.py

--- Python/Zestaw_2/2_11.py
+++ Python/Zestaw_2/2_11.py
@@ -3,6 +3,5 @@ znaki napisu oddzielone sa od siebie znakiem podlogi '_' """
 def split_letters(_string):
 	return '_'.join(_string[i] for i in range(0, len(_string)))
 
-
 if __name__ == "__main__":
 	print(split_letters("abcd"))
