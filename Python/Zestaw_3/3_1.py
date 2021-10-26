d020384239f4b343a9370871531683bbd3451554
Konrad <80765461+kon-pas@users.noreply.github.com>
2021-10-26 02:33:29 +0200

    Create 3_1.py

---
+++ Python/Zestaw_3/3_1.py
@@ -0,0 +1,16 @@
+"""
+x = 2; y = 3;
+if (x > y):
+	result = x;
+else:
+	result = y;
+	
+Komentarz: zbedne sredniki na koncach linii, zbedne nawiasy w instrukcji
+warunkowej if
+"""
+x = 2; y = 3
+if x > y:
+	result = x
+else:
+	result = y
+   
