3674aae6d753721742b4440e051aa33c0342bee3
Konrad <80765461+kon-pas@users.noreply.github.com>
2021-10-26 02:53:14 +0200

    Update 3_1.py

--- Python/Zestaw_3/3_1.py
+++ Python/Zestaw_3/3_1.py
@@ -1,4 +1,5 @@
-"""
+""" Podpunkt 1
+
 x = 2; y = 3;
 if (x > y):
 	result = x;
@@ -14,3 +15,23 @@ if x > y:
 else:
 	result = y
    
+""" Podpunkt 2
+
+for i in "qwerty": if ord(i) < 100: print (i)
+
+Komentarz: instrukcje powinny byc wypisane w osobnych liniach, odpowiednio 
+zagniezdzone uzywajac wciec tabulatorem
+"""
+for i in "qwerty":
+	if ord(i) < 100:
+		print (i)
+		
+""" Podpunkt 3
+
+for i in "axby": print (ord(i) if ord(i) < 100 else i)
+
+Komentarz: Poprawne, niemniej dobra praktyka jest pisanie instrukcji w 
+osobnej linii
+"""
+for i in "axby":
+	print (ord(i) if ord(i) < 100 else i)
