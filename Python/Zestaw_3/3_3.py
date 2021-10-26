5f90fed84e4e353db50368f73472da3c5fd0bde2
Konrad <80765461+kon-pas@users.noreply.github.com>
2021-10-26 03:57:37 +0200

    Create 3_3.py

---
+++ Python/Zestaw_3/3_3.py
@@ -0,0 +1,9 @@
+def get_string_numbers_except_mod_3(limit):
+	output = ''
+	for i in range(limit+1):
+		if i % 3 != 0:
+			output = output + str(i) + ' '
+	return output
+
+if __name__ == "__main__":
+  print(get_string_numbers_except_mod_3(30))
